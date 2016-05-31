b590a71fe39b9bb212316336c9e08f90d3699eaa
Maximilian Weber <weber.maximilian@googlemail.com>
2016-05-31 17:39:16 +0200

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sv/gcloud.client "0.1.3-SNAPSHOT"
+(defproject sv/gcloud.client "0.1.3"
   :description "A client to talk to the Google Cloud API (based on clj-http)."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
