f531e45bf9252833dfe497f32272885f321162e0
Maximilian Weber <weber.maximilian@googlemail.com>
2017-07-27 14:50:23 +0200

    Added `wrap-retry` middleware that retries failed requests which are retryable.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "2.0.0"]
                  [cheshire "5.5.0"]
+                 [listora/again "0.1.0"]
                  [com.google.api-client/google-api-client "1.19.1"]])
---
+++ src/sv/gcloud/client/middleware/retry.clj
@@ -0,0 +1,63 @@
+(ns sv.gcloud.client.middleware.retry
+  (:require [again.core :as again]))
+
+(defn default-retry-strategy
+  "The default retry strategy: an exponential-backoff with a
+   max-duration of 10 seconds and a maximum of 10 retries."
+  []
+  (again/max-duration
+   10000
+   (again/max-retries
+    10
+    (again/randomize-strategy
+     0.5
+     (again/multiplicative-strategy 500 1.5)))))
+
+(def http-status-too-many-requests 429)
+
+(defn http-status-5xx?
+  "Is the status code 5xx (500-599)"
+  [status-code]
+  (= (long (/ (long status-code) 100)) 5))
+
+(defn http-status-retry?
+  "Is the status code either a 5xx or a `too many request` (429)."
+  [status-code]
+  (or
+   (= status-code http-status-too-many-requests)
+   (http-status-5xx? status-code)))
+
+(defn retry?
+  "Default `retry?` for `wrap-retry` middleware. Is the status code
+   of the response either a 5xx or a `too many request` (429)."
+  [request response]
+  (http-status-retry? (:status response)))
+
+(defn- sleep
+  [delay]
+  (Thread/sleep (long delay)))
+
+(defn wrap-retry
+  "Middleware that retries the request, if (retry? request response)
+   returns true. The retry-strategy function should return a sequence
+   of millisecond amounts, for example: `[200 1000]` means wait 200 ms
+   after the first and 1 second after the second request.
+
+   Read more about retryable request in the context of Google Cloud
+   Platform here:
+
+   https://cloud.google.com/storage/docs/gsutil/addlhelp/RetryHandlingStrategy"
+  ([client retry-strategy retry?]
+   (fn [request]
+     (loop [retries (cons 0 (retry-strategy))]
+       (let [sleep-ms (first retries)]
+         (sleep sleep-ms)
+         (let [response (client request)]
+           (if (and (true? (retry? request response))
+                    (seq (rest retries)))
+             (recur (rest retries))
+             response))))))
+  ([client retry-strategy]
+   (wrap-retry client retry-strategy retry?))
+  ([client]
+   (wrap-retry client default-retry-strategy)))
